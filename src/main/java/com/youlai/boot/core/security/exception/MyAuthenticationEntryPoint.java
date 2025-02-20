ecdd9a1ffe7142cad094a94b05b06cc0718517c3
husong <1345001028@qq.com>
2025-02-20 17:13:47 +0800

    feat: 项目初始化

---
+++ src/main/java/com/youlai/boot/core/security/exception/MyAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package com.youlai.boot.core.security.exception;
+
+import com.youlai.boot.common.result.ResultCode;
+import com.youlai.boot.common.util.ResponseUtils;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * 认证异常处理
+ *
+ * @author Ray.Hao
+ * @since 2.0.0
+ */
+@Component
+public class MyAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        int status = response.getStatus();
+        if (status == HttpServletResponse.SC_NOT_FOUND) {
+            // 资源不存在
+            ResponseUtils.writeErrMsg(response, ResultCode.USER_RESOURCE_NOT_FOUND);
+        } else {
+            if (authException instanceof UsernameNotFoundException || authException instanceof BadCredentialsException) {
+                // 用户名或密码错误
+                ResponseUtils.writeErrMsg(response, ResultCode.USER_PASSWORD_ERROR);
+            } else {
+                // 未认证或者token过期
+                ResponseUtils.writeErrMsg(response, ResultCode.ACCESS_TOKEN_INVALID);
+            }
+        }
+    }
+}
